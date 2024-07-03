238d4ff183d7d614df863c531d7f58b29e7f32da
Hasen <79167115+hasenoz@users.noreply.github.com>
2024-07-03 14:42:15 +0300

    key hold

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,18 @@ public class PlayerController : MonoBehaviour
 {
     // degiskenler
 
+    // Hareket hýzý - [SerializeField] Editörde görünmesini saðlar
+    [SerializeField] float moveSpeed = 2f;
     // InputAction yeni bir input alma classý oklar ve wasd üzerinden calýsýyor
     InputAction moveAction;
-    
+    InputAction collectAction;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
+        collectAction = InputSystem.actions.FindAction("Interact");
     }
 
     // Update is called once per frame
@@ -20,7 +24,25 @@ public class PlayerController : MonoBehaviour
     {
         // moveActiona göre karakteri hareket ettir
         Vector2 moveValue = moveAction.ReadValue<Vector2>();
-        transform.Translate(moveValue*Time.deltaTime);
-        
+        transform.Translate(moveValue * Time.deltaTime * moveSpeed);
+        CollectKey();
+
+    }
+
+    void CollectKey()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.zero);
+        if (hit.collider != null)
+        {
+            Debug.Log("Vurdum");
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("key") && collectAction.IsPressed())
+        {
+            collision.transform.parent = transform;
+        }
     }
 }
